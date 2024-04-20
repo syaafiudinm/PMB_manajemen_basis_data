55476520daf01598a0510e1411c3d4f11a7962b0
syaafiudinm <altafpasallo12@gmail.com>
2024-04-20 21:23:10 +0800

    database connection and first migration

---
+++ database/migrations/2024_04_20_054502_create_peserta_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('peserta', function (Blueprint $table) {
+            $table->id();
+            $table->string('NISN');
+            $table->string('nama');
+            $table->string('Fakultas');
+            $table->string('Jurusan');
+            $table->string('asalSekolah');
+            $table->date('tgl_lahir');
+            $table->string('Phone');
+            $table->string('Email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('peserta');
+    }
+};
